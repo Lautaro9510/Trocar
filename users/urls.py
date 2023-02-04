a15801a241922eaf5f03de705627bc5ad1fbb80f
lautarofasc <lautaro9510@gmail.com>
2023-02-04 00:39:05 -0300

    Se agregan estilos al login

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from users.views import login_view, register, update_user, update_user_profile
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
+    path('login/', login_view),
     path('logout/', LogoutView.as_view(template_name = 'users/logout.html')),
-    path('signup/', register, name = 'register'),
-    path('update/', update_user, name = 'update_user'),
-    path('update/profile/', update_user_profile, name = 'update_user_profile'),
+    path('register/', register),
+    path('update/', update_user),
+    path('update/profile/', update_user_profile),
 ]
