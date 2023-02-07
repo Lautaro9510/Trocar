52b5a864d47d4fd4dae7ce56638e7f8210e3ef08
lautarofasc <lautaro9510@gmail.com>
2023-02-07 00:59:55 -0300

    Se crean profiles por defecto

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 
 # Create your models here.
 from django.contrib.auth.models import User
@@ -7,4 +8,14 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     phone = models.CharField(max_length=25, null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_images', null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_images', null=True, default="user_default.png")
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
